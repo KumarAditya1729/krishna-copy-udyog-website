c71f7515d3192c1c76af008825608ee628fea633
adityashrivastavaaaaaaa-lang <arkashrios@gmail.com>
2026-04-29 14:52:07 +0530

    fix: correct Vercel outputDirectory to dist/client

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from '../components/site/Home';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
